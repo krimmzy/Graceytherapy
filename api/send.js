56c3fac9e347f6fb3b682c9da3cde9598299c1b7
Akorede Lemo <125971535+krimmzy@users.noreply.github.com>
2025-05-27 16:08:15 -0700

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,44 +1,33 @@
-// /api/send.js
-import nodemailer from 'nodemailer';
+const nodemailer = require("nodemailer");
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
 
-  const {
-    booking_time,
-    location_type,
-    massage_type,
-    client_location,
-    preferred_name,
-    meeting_point,
-    law_enforcement,
-    disease,
-    payment_method
-  } = req.body;
+  const body = req.body;
 
-  const bookingId = 'GTY-' + Math.floor(100000 + Math.random() * 900000);
+  const bookingId = "GTY-" + Math.floor(100000 + Math.random() * 900000);
 
   const emailBody = `
 Booking ID: ${bookingId}
-Time: ${booking_time}
-In/Outcall: ${location_type}
-Massage Type: ${massage_type}
-Client Location: ${client_location}
-Preferred Name: ${preferred_name}
-Meeting Point: ${meeting_point}
-Law Enforcement Affiliation: ${law_enforcement}
-Disease: ${disease}
-Payment Method: ${payment_method}
+Time: ${body.booking_time}
+In/Outcall: ${body.location_type}
+Massage Type: ${body.massage_type}
+Client Location: ${body.client_location}
+Preferred Name: ${body.preferred_name}
+Meeting Point: ${body.meeting_point}
+Law Enforcement Affiliation: ${body.law_enforcement}
+Disease: ${body.disease}
+Payment Method: ${body.payment_method}
   `;
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASS,
-    }
+    },
   });
 
   const mailOptions = {
@@ -50,7 +39,9 @@ Payment Method: ${payment_method}
 
   try {
     await transporter.sendMail(mailOptions);
-    res.writeHead(302, { Location: `/thankyou.html?booking_id=${bookingId}` });
+    res.writeHead(302, {
+      Location: `/thankyou.html?booking_id=${bookingId}`,
+    });
     res.end();
   } catch (error) {
     res.status(500).send("Failed to send email: " + error.message);
